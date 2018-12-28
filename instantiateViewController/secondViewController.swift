4127211fdd02f6e6a4fa85cbfbf8dd47ce510a11
Shama Ahlawat <shama@oriserve.com>
2018-12-28 10:35:25 +0530

    initial setup

---
+++ instantiateViewController/secondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  secondViewController.swift
+//  instantiateViewController
+//
+//  Created by Shama Ahlawat on 28/12/18.
+//  Copyright © 2018 oriserve. All rights reserved.
+//
+
+import Foundation
+
+public class ChatbotService {
+    
+    public static func doSomething() -> String {
+    return "do something for us"
+ }
+}
