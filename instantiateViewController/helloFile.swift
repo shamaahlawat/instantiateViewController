16737789db21bdb91fccb840add1f646678758da
Shama Ahlawat <shama@oriserve.com>
2019-01-10 13:12:01 +0530

    webview added

--- instantiateViewController/helloFile.swift
+++ instantiateViewController/helloFile.swift
@@ -10,8 +10,21 @@ import Foundation
 import UIKit
 
 class helloFile : UIViewController {
+    @IBOutlet internal var web: UIWebView!
     
     override func viewDidLoad() {
         print("i am here")
+        do
+        {
+            let testHTML = Bundle.main.path(forResource: "index", ofType: "html")
+            let contents =  try NSString(contentsOfFile: testHTML!, encoding: String.Encoding.utf8.rawValue)
+            let baseUrl = NSURL(fileURLWithPath: testHTML!) //for load css file
+            
+            web.loadHTMLString(contents as String, baseURL: baseUrl as URL)
+        }
+        catch
+        {
+            
+        }
     }
 }
