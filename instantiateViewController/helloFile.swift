4d8c5f063284988e845ba56acdecd05c6b1756f7
Shama Ahlawat <shama@oriserve.com>
2019-01-10 12:55:18 +0530

    hello file added

---
+++ instantiateViewController/helloFile.swift
@@ -0,0 +1,17 @@
+//
+//  helloFile.swift
+//  instantiateViewController
+//
+//  Created by Shama Ahlawat on 10/01/19.
+//  Copyright © 2019 oriserve. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class helloFile : UIViewController {
+    
+    override func viewDidLoad() {
+        print("i am here")
+    }
+}
